53f15a649263914a1ddd22120d8a73de0c6b947a
Jaime Hernández <jaimevazz@gmail.com>
2017-08-07 17:24:04 +0100

    Added bowser
    + Added browser detection to display full features.
    (Bowser.js)

---
+++ js/browser.js
@@ -0,0 +1,5 @@
+if (bowser.chrome && bowser.version >= 48) {
+  alert('Welcome to the full featured Radian web site!');
+} else {
+	alert('Limited features of this site. Please download the latest Google Chrome version.');
+}
