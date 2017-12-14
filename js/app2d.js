71bec29c1de119ea09a5871f162e4a889cbb8bb4
Jaime Hernández <jaimevazz@gmail.com>
2017-12-13 18:20:02 -0600

    Begin grid effect. - Cleaned code. - Purged textures. - Begin grid CSS effect.

--- js/app2d.js
+++ js/app2d.js
@@ -18,61 +18,60 @@ var team;
 var count = 0;
 
 //SHADER_LOADER.load(
-function shaderLoad(data) {
-    shaderCode = data.smoke.fragment;
-    // v3
-    smokeShader = new PIXI.AbstractFilter(null, shaderCode, uniforms);
-    // v4
-    //smokeShader = new PIXI.Filter(null, shaderCode, uniforms);
-    bg.filters = [smokeShader];
-    bg.width = width;
-    bg.height = height;
-    stage.addChild(bg);
-    // Team silhouette
-    team = PIXI.Sprite.fromImage("textures/pixi/siluetas.png");
-    team.anchor.set(0.5, 0.9);
-    var textureRatio = 1920 / 441;
-    var newWidth = pixi_renderer.width < 1280 ? 1280 : pixi_renderer.width;
-    team.width = newWidth;
-    team.height = newWidth / textureRatio * 1.1;
-    team.x = pixi_renderer.width / 2;
-    team.y = pixi_renderer.height;
-    stage.addChild(team);
+function OnShadersLoaded2D(data) {
+  shaderCode = data.smoke.fragment;
+  // v3
+  smokeShader = new PIXI.AbstractFilter(null, shaderCode, uniforms);
+  // v4
+  //smokeShader = new PIXI.Filter(null, shaderCode, uniforms);
+  bg.filters = [smokeShader];
+  bg.width = width;
+  bg.height = height;
+  stage.addChild(bg);
+  // Team silhouette
+  team = PIXI.Sprite.fromImage("textures/pixi/siluetas.png");
+  team.anchor.set(0.5, 0.9);
+  var textureRatio = 1920 / 441;
+  var newWidth = pixi_renderer.width < 1280 ? 1280 : pixi_renderer.width;
+  team.width = newWidth;
+  team.height = newWidth / textureRatio * 1.1;
+  team.x = pixi_renderer.width / 2;
+  team.y = pixi_renderer.height;
+  stage.addChild(team);
 }
 //);
 function rendererResize() {
-    console.log("App 2D: Resize handler called.");
-    var screenWidth = window.innerWidth;
-    var screenHeight = window.innerHeight;
-    pixi_renderer.resize(screenWidth, screenHeight);
-    bg.width = screenWidth;
-    bg.height = screenHeight;
-    //uniforms.resolution = {type: "v2", value: {x: screenWidth, y: screenHeight}};
-    smokeShader.uniforms.resolution = {x: screenWidth, y: screenHeight};
-    team.x = screenWidth / 2;
+  console.log("App 2D: Resize handler called.");
+  var screenWidth = window.innerWidth;
+  var screenHeight = window.innerHeight;
+  pixi_renderer.resize(screenWidth, screenHeight);
+  bg.width = screenWidth;
+  bg.height = screenHeight;
+  //uniforms.resolution = {type: "v2", value: {x: screenWidth, y: screenHeight}};
+  smokeShader.uniforms.resolution = {x: screenWidth, y: screenHeight};
+  team.x = screenWidth / 2;
 }
 function pixi_init() {
-    //The stage is the root container that will hold everything in our scene
-    stage = new PIXI.Container();
-    shaderContainer = document.getElementById("app2d_cont");
-    //Chooses either WebGL if supported or falls back to Canvas rendering
-    pixi_renderer = new PIXI.autoDetectRenderer(width, height, {antialias: true, autoResize: true, transparent: true, resolution: 1});
-    //pixi_renderer = new PIXI.autoDetectRenderer(width, height, shaderContainer, true);
-    pixi_renderer.view.className = "pixi";
-    //Add the render view object into the page
-    shaderContainer.appendChild(pixi_renderer.view);
-    bg = PIXI.Sprite.fromImage("textures/pixi/pixi.png");
-    // Bind Events
-    window.addEventListener("resize", rendererResize, false);
-
+  //The stage is the root container that will hold everything in our scene
+  stage = new PIXI.Container();
+  shaderContainer = document.getElementById("app2d_cont");
+  //Chooses either WebGL if supported or falls back to Canvas rendering
+  pixi_renderer = new PIXI.autoDetectRenderer(width, height, {antialias: true, autoResize: true, transparent: true, resolution: 1});
+  //pixi_renderer = new PIXI.autoDetectRenderer(width, height, shaderContainer, true);
+  pixi_renderer.view.className = "pixi";
+  //Add the render view object into the page
+  shaderContainer.appendChild(pixi_renderer.view);
+  bg = PIXI.Sprite.fromImage("textures/pixi/pixi.png");
+  // Bind Events
+  window.addEventListener("resize", rendererResize, false);
 }
 function pixi_animate() {
-    // start the timer for the next animation loop
-    requestAnimationFrame(pixi_animate);
-    count += 0.01;
-    if (typeof smokeShader != "undefined") {
-        smokeShader.uniforms.time.value = count;
-    }
-    // this is the main render call that makes pixi draw your container and its children.
-    pixi_renderer.render(stage);
+  // start the timer for the next animation loop
+  requestAnimationFrame(pixi_animate);
+  count += 0.01;
+  if (typeof smokeShader != "undefined") {
+    smokeShader.uniforms.time.value = count;
+  }
+  // this is the main render call that makes pixi draw your container and its children.
+  pixi_renderer.render(stage);
 }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // GLOBAL CONTROL
-var debug = false;
+var debug = true;
 // TODO: Fix this temporal globals
 // Interface with jQueryExtents (Animation)
 var isPlaying = false;
@@ -9,94 +9,100 @@ var imageNames = ["French.jpg","Dog.jpg","Chilaquil.jpg","Pug.jpg"];
 var isLoaded = false;
 var loadBar;
 
+var fullPageParams = {
+  sectionsColor: ['rgba(0,0,0,0)'],
+  anchors: ['aboutus', 'edesign', 'vfx', 'videogames', 'contact'],
+  recordHistory: false,
+  menu: '#menu',
+  scrollingSpeed: 2000,
+  onLeave: function(index, nextIndex, direction){
+    //console.log("onLeave--" + "index: " + index + " nextIndex: " + nextIndex + " direction: " +  direction);
+    hideBigImage();
+  },
+  afterRender: function(){
+    //console.log("afterRender");
+  },
+  afterResize: function(){
+    //console.log("afterResize");
+  },
+  afterLoad: function(anchorLink, index){
+    //console.log("afterLoad--" + "anchorLink: " + anchorLink + " index: " + index );
+    if (isLoaded) {
+      triggerAnim(1);
+    }
+  }
+};
+
 // LOAD MANAGER (THREE)
 var manager = new THREE.LoadingManager();
-manager.onStart = function( item, loaded, total ) {
-    console.log( 'Loading started' );
-};
-manager.onLoad = function() {
-    $('#logo').hover(function(){
-    	$('#loadScreen').animateOnce('fadeOut');
-    	$.fn.fullpage.setAllowScrolling(true);
-    });
-    $('#loadBar').animateOnce('fadeOut');
-    $('#logo').animateOnce('tada');
-    console.log('Loading complete');
-    if (!debug) {
-    	$('#fullpage').fullpage.moveTo('aboutus');
-    }
-    isLoaded = true;
-    TriggerAnim(1);
-};
-manager.onProgress = function( item, loaded, total ) {
-    //console.log( item, loaded, total );
-    loadBar.set((loaded*100.0)/total*1.0)
-};
-manager.onError = function( url ) {
-    console.log( 'Error loading' );
-};
-
-// MAIN ON LOADED (Javascript method)
-window.addEventListener( 'load', OnLoaded, false );
-function OnLoaded() {
-	loadBar = document.getElementById('loadBar').ldBar;
-	fullpage_init();
-	init();
-	pixi_init();
-	//if (debug) initGUI();
-	animate();
-	pixi_animate();
+manager.onStart = onManagerStart;
+manager.onLoad = onManagerLoad;
+manager.onProgress = onManagerProgress;
+manager.onError = onManagerError;
+// Manager Handlers
+function onManagerStart (item, loaded, total) {
+  console.log( 'Loading started' );
 }
-
+function onManagerLoad () {
+  console.log('Loading complete');
+  $('#logo').hover(OnEnterPage);
+  $('#loadBar').animateOnce('fadeOut');
+  $('#logo').animateOnce('tada');
+  if (!debug) {
+    $('#fullpage').fullpage.moveTo('aboutus');
+  }
+  isLoaded = true;
+}
+function onManagerProgress (item, loaded, total) {
+  loadBar.set((loaded*100.0)/total*1.0)
+}
+function onManagerError (url) {
+  console.log( 'Error loading' );
+}
+function OnEnterPage ()
+{
+  $('#loadScreen').animateOnce('fadeOut');
+  $.fn.fullpage.setAllowScrolling(true);
+  //if (debug) initGUI();
+  animate();
+  pixi_animate();
+  triggerAnim(1);
+}
+// MAIN ON LOADED (Javascript method)
+window.addEventListener( 'load', onWindowLoaded, false );
+function onWindowLoaded() {
+  loadBar = document.getElementById('loadBar').ldBar;
+  fullpage_init();
+  init();
+  pixi_init();
+}
 // ON LOADED (Jquery ready method)
 //$(document).ready(function() {
 function fullpage_init() {
-	$('#fullpage').fullpage({
-		sectionsColor: ['rgba(0,0,0,0)'],
-		anchors: ['aboutus', 'edesign', 'vfx', 'videogames', 'contact'],
-		recordHistory: false,
-		menu: '#menu',
-    scrollingSpeed: 2000,
-		onLeave: function(index, nextIndex, direction){
-			//console.log("onLeave--" + "index: " + index + " nextIndex: " + nextIndex + " direction: " +  direction);
-			hideBigImage();
-		},
-		afterRender: function(){
-			//console.log("afterRender");
-		},
-		afterResize: function(){
-			//console.log("afterResize");
-		},
-		afterLoad: function(anchorLink, index){
-			//console.log("afterLoad--" + "anchorLink: " + anchorLink + " index: " + index );
-			if (isLoaded) {
-				TriggerAnim(1);
-			}
-		}
-	});
-//});
+  $('#fullpage').fullpage(fullPageParams);
+  //});
 }
-
+// Description images behaviour
 function toggleBigImage(index) {
-	if (!isPlaying) {
-		if (imageActive) hideBigImage();
-		else showBigImage(index);
-	}
+  if (!isPlaying) {
+    if (imageActive) hideBigImage();
+    else showBigImage(index);
+  }
 }
 function showBigImage(index) {
-	if (!imageActive) {
-		var imageName = imageNames[index];
-		$('#bigImage').css({
-        	"background-image": "url('images/"+imageName+"')"
-        }).animateOnce('fadeInRight');
-		$( '.bigImage_toggle' ).text( "Close project" );
-		imageActive = true;
-	}
+  if (!imageActive) {
+    var imageName = imageNames[index];
+    $('#bigImage').css({
+      "background-image": "url('images/"+imageName+"')"
+    }).animateOnce('fadeInRight');
+    $( '.bigImage_toggle' ).text( "Close project" );
+    imageActive = true;
+  }
 }
 function hideBigImage() {
-	if (imageActive) {
-		$('#bigImage').animateOnce('fadeOutRight');
-		$( '.bigImage_toggle' ).text( "View project" );
-		imageActive = false;
-	}
+  if (imageActive) {
+    $('#bigImage').animateOnce('fadeOutRight');
+    $( '.bigImage_toggle' ).text( "View project" );
+    imageActive = false;
+  }
 }
